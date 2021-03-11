085806c8384f919c0e01ed6aaab74dcbeb0a80eb
Natasha Colusso <natashacolusso@gmail.com>
2021-03-11 10:04:53 -0800

    feature: udpated template for polls and route

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from django.template import loader
 
 from .models import Question
@@ -14,11 +14,13 @@ def index(request):
   return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-  try:
-    question = Question.objects.gete(pk=question_id)
-  except Question.DoesNotExist:
-    raise Http404("Question does not exist")
-  return render(request, 'polls/details.html', {'question': question})
+  # try:
+  #   question = Question.objects.gete(pk=question_id)
+  # except Question.DoesNotExist:
+  #   raise Http404("Question does not exist")
+  # return render(request, 'polls/details.html', {'question': question})
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
   response = "You're looking at the results of question %s."
