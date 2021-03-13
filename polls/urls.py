15f76d7533c007c05d073ccec130f1e8a3d8ebbb
Natasha Colusso <natashacolusso@gmail.com>
2021-03-13 09:26:19 -0800

    Feature: Working form to add question and choices to db

--- polls/urls.py
+++ polls/urls.py
@@ -6,10 +6,14 @@ app_name = 'polls'
 urlpatterns = [
   # ex: /polls/
   path('', views.IndexView.as_view(), name="index"),
+  # ex: /polls/new
+  path('create/', views.add_question, name="create"),
   #ex: polls/5/
   path('<int:pk>/', views.DetailView.as_view(), name='detail'),
   # ex: /polls/5/results/
   path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+  # ex: /polls/5/delete/
+  path('<int:pk>/delete/', views.DeleteView.as_view(), name='delete'),
   # ex: /polls/5/vote/
   path('<int:question_id>/vote/', views.vote, name='vote')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views import generic
+from django.contrib import messages
+from .forms import QuestionForm
 
-from .models import Question
+from extra_views import CreateWithInlinesView, UpdateWithInlinesView, InlineFormSetFactory
+
+from .models import Question, Choice
 
 class IndexView(generic.ListView):
   template_name = 'polls/index.html'
@@ -20,6 +24,53 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
   model = Question
   template_name = 'polls/results.html'
+
+class DeleteView(generic.DeleteView):
+  model = Question
+  template_name = 'polls/delete.html'
+  context_object_name = 'total_votes'
+  success_url = reverse_lazy('polls:index')
+
+  def get_total_votes(self):
+    total_votes = 0
+    for choice in Question.choice_set.all:
+      total_votes += choice.votes
+      print(total_votes)
+    return "TEsTING"
+
+  total_votes = get_total_votes
+
+def add_question(request):
+  if request.method == 'POST':
+   
+    choices = []
+    choice1 = request.POST.get('choice1')
+    choice2 = request.POST.get('choice2')
+    choice3 = request.POST.get('choice3')
+    choice4 = request.POST.get('choice4')
+    if (choice1):
+      choices.append(choice1)
+    if (choice2):
+      choices.append(choice2)
+    if (choice3):
+      choices.append(choice3)
+    if (choice4):
+      choices.append(choice4)
+
+    question_form = QuestionForm(request.POST, instance=Question())
+
+    if question_form.is_valid():
+      new_question = question_form.save()
+      print(new_question.id)
+      for c in choices:
+        Choice.objects.create(choice_text=c, question=new_question)
+      return HttpResponseRedirect('/polls/')
+
+  else:
+    question_form = QuestionForm(request.POST, instance=Question())
+    return render(request, 'polls/create.html', {'q_form': question_form})
+    
+
 
 def vote(request, question_id):
   question = get_object_or_404(Question, pk=question_id)
